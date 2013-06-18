dd6b00fec4f138aa129ca76d6223884920d23bdd
Vegard S <jvstene@gmail.com>
2013-06-18 10:46:12 +0200

    Added Norwegian translation

--- lang/no.js
+++ lang/no.js
@@ -2,9 +2,6 @@
 CKEDITOR.plugins.setLang('wordcount', 'no', {
     WordCount: 'Ord:',
     CharCount: 'Tegn:',
-    CharCountWithHTML: 'Tegn (including HTML):',
-    Paragraphs: 'Paragraphs:',
-    pasteWarning: 'Content can not be pasted because it is above the allowed limit',
-    Selected: 'Selected: ',
+    limit: 'Grense:',
     title: 'Statistikk'
 });
