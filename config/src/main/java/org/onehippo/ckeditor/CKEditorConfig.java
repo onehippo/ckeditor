a2db22b80164bc2a396f67521cf753e89ca1657c
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-03-21 09:17:38 +0100

    CMS-10666 Add config module with default editor config for Hippo CMS
    Also moved the hippostyles.js and hippocontents.css files that are
    referenced in the config.

---
+++ config/src/main/java/org/onehippo/ckeditor/CKEditorConfig.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onehippo.ckeditor;
+
+import java.util.Locale;
+
+/**
+ * Default configuration of CKEditor fields in Hippo CMS.
+ */
+public class CKEditorConfig {
+
+    private CKEditorConfig() {
+    }
+
+    public static final String DEFAULT_FORMATTED_TEXT_CONFIG = "{"
+            // do not html encode but utf-8 encode hence entities = false
+            + "  entities: false,"
+            // &gt; must not be replaced with > hence basicEntities = true
+            + "  basicEntities: true,"
+            + "  autoUpdateElement: false,"
+            + "  contentsCss: ['ckeditor/hippocontents.css'],"
+            + "  plugins: 'basicstyles,button,clipboard,contextmenu,divarea,enterkey,entities,floatingspace,floatpanel,htmlwriter,listblock,magicline,menu,menubutton,panel,panelbutton,removeformat,richcombo,stylescombo,tab,toolbar,undo',"
+            + "  title: false,"
+            + "  toolbar: ["
+            + "    { name: 'styles', items: [ 'Styles' ] },"
+            + "    { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', '-', 'RemoveFormat' ] },"
+            + "    { name: 'clipboard', items: [ 'Undo', 'Redo' ] }"
+            + "  ]"
+            + "}";
+
+    public static final String DEFAULT_RICH_TEXT_CONFIG = "{"
+            // do not html encode but utf-8 encode hence entities = false
+            + "  entities: false,"
+            // &gt; must not be replaced with > hence basicEntities = true
+            + "  basicEntities: true,"
+            + "  autoUpdateElement: false,"
+            + "  contentsCss: ['ckeditor/hippocontents.css'],"
+            + "  dialog_buttonsOrder: 'ltr',"
+            + "  dialog_noConfirmCancel: true,"
+            + "  extraAllowedContent: 'embed[allowscriptaccess,height,src,type,width]; img[border,hspace,vspace]; object[align,data,height,id,title,type,width]; p[align]; param[name,value]; table[width]; td[valign,width]; th[valign,width];',"
+            + "  keystrokes: ["
+            + "    [ 'Ctrl', 'm', 'maximize' ],"
+            + "    [ 'Alt', 'b', 'showblocks' ]"
+            + "  ],"
+            + "  linkShowAdvancedTab: false,"
+            + "  plugins: 'a11yhelp,basicstyles,button,clipboard,codemirror,contextmenu,dialog,dialogadvtab,dialogui,divarea,elementspath,enterkey,entities,floatingspace,floatpanel,htmlwriter,indent,indentblock,indentlist,justify,link,list,listblock,liststyle,magicline,maximize,menu,menubutton,panel,panelbutton,pastefromword,pastetext,popup,removeformat,resize,richcombo,showblocks,showborders,specialchar,stylescombo,tab,table,tableresize,tabletools,textselection,toolbar,undo,youtube',"
+            + "  removeFormatAttributes: 'style,lang,width,height,align,hspace,valign',"
+            + "  title: false,"
+            + "  toolbarGroups: ["
+            + "    { name: 'styles' },"
+            + "    { name: 'basicstyles' },"
+            + "    { name: 'undo' },"
+            + "    { name: 'listindentalign',  groups: [ 'list', 'indent', 'align' ] },"
+            + "    { name: 'links' },"
+            + "    { name: 'insert' },"
+            + "    { name: 'tools' },"
+            + "    { name: 'mode' }"
+            + "  ]"
+            + "}";
+
+    private static final String STYLES_SET_LANGUAGE_PARAM = "{language}";
+
+    public static String getStylesSet(final String customStylesSet, final Locale locale) {
+        return customStylesSet.replace(STYLES_SET_LANGUAGE_PARAM, locale.getLanguage());
+    }
+
+    public static String getDefaultStylesSet(final Locale locale) {
+        final String styleName = "hippo_" + locale.getLanguage();
+        return styleName + ":./hippostyles.js";
+    }
+
+
+}
